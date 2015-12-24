fa166f957286c7b226b4d4433723945ae3d59bd0
Danny Smith <danny@dasmith.co.uk>
2015-12-24 00:20:41 +0000

    Added footer to layout

--- spec/redirection_spec.rb
+++ spec/redirection_spec.rb
@@ -3,7 +3,7 @@ describe "login page" do
   it 'should redirect from homepage to login page' do
     get '/'
     expect(last_response.status).to eq 302
-    expect(last_response.header["Location"]).to match(%r{\/login$})
+    expect(last_response.header["Location"]).to match /\/login$/
   end
 
   it 'should display login page' do
